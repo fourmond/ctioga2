40a8bb4a4ba037ea397f20adfb4c944f29b61d35
fourmond <fourmond@86c91514-0d3b-42e0-aed1-4dfce116d947>
2010-01-26 22:05:39 +0000

    [ctioga2] And a quick use of this new feature to see tests real fast
    git-svn-id: svn+ssh://rubyforge.org/var/svn/ctioga2/trunk/ctioga2@141 86c91514-0d3b-42e0-aed1-4dfce116d947

--- tests/test-include.sh
+++ tests/test-include.sh
@@ -2,5 +2,10 @@
 # Copyright 2009 by Vincent Fourmond.
 # You can do whatever you want with this file.
 
+if [ -z $NOXPDF ]; then
+    ct_xpdf="-X"
+else
+    ct_xpdf=""
+fi
 # The way to invoque ctioga2
-ct="ctioga2 -X --echo --math"
+ct="ctioga2 $ct_xpdf --echo --math $CT_ADD "
