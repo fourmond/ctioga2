13dc2769df237b35f21891de5e6cf2108031a945
fourmond <fourmond@86c91514-0d3b-42e0-aed1-4dfce116d947>
2010-04-06 20:37:49 +0000

    [ctioga2] New tests for grids
    git-svn-id: svn+ssh://rubyforge.org/var/svn/ctioga2/trunk/ctioga2@148 86c91514-0d3b-42e0-aed1-4dfce116d947

---
+++ tests/grid.sh
@@ -0,0 +1,20 @@
+# grid.sh: demonstration of the new grid layout
+# Copyright 2010 by Vincent Fourmond
+# 
+# This file is provided as an example of how to use ctioga2. As such,
+# you can do whatever you wish with this file.
+
+# Include the definition of ct
+. ./test-include.sh
+
+# Plain grid
+$ct --setup-grid 2x2 --inset grid:1,0 'sin(x)' \
+    --next-inset grid:0,1 'x**2'
+
+# Non-uniform cell sizes
+$ct --setup-grid 1,2x2,1 --inset grid:1,0 'sin(x)' \
+    --next-inset grid:0,1 'x**2'
+
+# Additional specification
+$ct --setup-grid 1,2x2,1 --inset grid:1,0,xl=0.3 'sin(x)' \
+    --next-inset grid:0,1,yt=0.7,xr=0.9 'x**2'
