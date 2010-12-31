9912950a8e0efcfdd98b21f236eb99e230d8a74e
fourmond <fourmond@86c91514-0d3b-42e0-aed1-4dfce116d947>
2010-12-31 15:18:22 +0000

    [ctioga2] another test
    git-svn-id: svn+ssh://rubyforge.org/var/svn/ctioga2/trunk/ctioga2@218 86c91514-0d3b-42e0-aed1-4dfce116d947

--- tests/insets.sh
+++ tests/insets.sh
@@ -12,5 +12,10 @@ $ct 'x**2' --inset 'tl:0.2,0.9:0.4,0.3' \
     'cos(x)'
 
 # Oversimplified
-$ct 'x**2' --inset 'cc:0.3' \
-    'cos(x)'
+$ct 'x**2' --inset 'cc:0.35' 'cos(x)'
+
+# Same thing with a range specification, just to check it works fine !
+$ct 'x**2' --inset 'cc:0.35' 'cos(x)' --yrange -2:2
+
+# $ct 'x**2' '1 - cos(x)' --zoom-inset 'cc:0.3' \
+#     --yrange -1:1 
