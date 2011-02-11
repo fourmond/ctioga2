1a6e3b371526403b1906f736bc716bae83f4f73a
fourmond <fourmond@86c91514-0d3b-42e0-aed1-4dfce116d947>
2011-02-11 21:12:35 +0000

    [ctioga2] Came up with a portable way to get the SVN revision, even when using the git mirror
    git-svn-id: svn+ssh://rubyforge.org/var/svn/ctioga2/trunk/ctioga2@281 86c91514-0d3b-42e0-aed1-4dfce116d947

--- pre-install.rb
+++ pre-install.rb
@@ -1,18 +1,33 @@
-# If we are managed by git-svn and not svn, we provide SVN information
-# on the fly.
+# We come up with a "SVN revision"
 if File.directory?("#{srcdir_root}/.git")
-  svn_info = `cd #{srcdir_root}; git svn info`
-  svn_info =~ /Last\s+Changed\s+Date\s*:\s*(.*)/
-  date = $1
-  svn_info =~ /Last\s+Changed\s+Rev\s*:\s*(.*)/
-  rev = $1
+  git_log = File.popen("cd #{srcdir_root}; git log")
+  date = nil
+  svn_rev = nil
+  svn_age = -1                   # Number of commits posterior to last SVN
+  git_commit = nil
+  while l = git_log.gets
+    case l 
+    when /commit\s+(\S+)/
+      git_commit = $1
+      svn_age += 1
+    when /Date:\s*(.*)/
+      date ||= $1
+    when /git-svn-id:\s*\S+@(\d+)/
+      svn_rev = $1.to_i
+      git_log.close
+      break
+    end
+  end
+  if svn_age > 0
+    svn_rev = "#{svn_rev}+git#{svn_age}"
+  end
   File.open("lib/ctioga2/git-fools-svn.rb", "w") do |f|
     f.puts <<"EOF"
 # Automatically generated file.
 module CTioga2
 
 
-   Version::register_svn_info('$Revision: #{rev}$', 
+   Version::register_svn_info('$Revision: #{svn_rev}$', 
                               '$Date: #{date}$')
 end
 EOF
