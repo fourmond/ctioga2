91cd114bc7b53fc8a54f839079651854c883a088
fourmond <fourmond@86c91514-0d3b-42e0-aed1-4dfce116d947>
2012-12-23 21:45:15 +0000

    [ctioga2] Demonstration of the use of stylesheets in complex graphs
    git-svn-id: svn+ssh://rubyforge.org/var/svn/ctioga2/trunk/ctioga2@343 86c91514-0d3b-42e0-aed1-4dfce116d947

--- tests/grid.sh
+++ tests/grid.sh
@@ -18,3 +18,11 @@ $ct --setup-grid 1,2x2,1 --inset grid:1,0 'sin(x)' \
 # Additional specification
 $ct --setup-grid 1,2x2,1 --inset grid:1,0,xl=0.3 'sin(x)' \
     --next-inset grid:0,1,yt=0.7,xr=0.9 'x**2'
+
+# Use of style sheets to simplify the handling of background/axes and
+# the like
+$ct --setup-grid 1,2x2,1 \
+    --default-background-style background /background_color=Pink \
+    /watermark='two line\\watermark' \
+    --inset grid:1,0 'sin(x)' \
+    --next-inset grid:0,1 'x**2'
