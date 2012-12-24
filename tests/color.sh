f94cb8d6b673b43933e8fa0777219aa63bc8f8ed
fourmond <fourmond@86c91514-0d3b-42e0-aed1-4dfce116d947>
2012-12-24 09:43:41 +0000

    [ctioga2] add a test for different color schemes
    git-svn-id: svn+ssh://rubyforge.org/var/svn/ctioga2/trunk/ctioga2@346 86c91514-0d3b-42e0-aed1-4dfce116d947

---
+++ tests/color.sh
@@ -0,0 +1,15 @@
+# color.sh: all ways to specify colors
+# Copyright 2012 by Vincent Fourmond
+# 
+# You can do whatever you want with this file, including removing the
+# copyright notice and this text.
+
+. ./test-include.sh
+
+$ct --legend-inside br \
+    'x' /color=Red /legend='Tioga color: Red' \
+    '2*x' /color=\#729 /legend='HTML syntax: \#729' \
+    '3*x' /color=0.1,0.9,0.2 /legend='RGB fractions: 0.1,0.9,0.2' \
+    '0.5*x' /color=hls:0.1,0.9,0.2 /legend='HLS fractions: hls:0.1,0.9,0.2' \
+    '0.1*x' /color='#729!50!Green' /legend='xcolor mix: \#729!50!Green'
+
