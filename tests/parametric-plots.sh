5a9ed42bce830c976111c5a0e8b331daa6e7a8f8
fourmond <fourmond@86c91514-0d3b-42e0-aed1-4dfce116d947>
2013-09-18 22:43:30 +0000

    [ctioga2] new tests for the parametric plots
    git-svn-id: svn+ssh://rubyforge.org/var/svn/ctioga2/trunk/ctioga2@514 86c91514-0d3b-42e0-aed1-4dfce116d947

---
+++ tests/parametric-plots.sh
@@ -0,0 +1,24 @@
+# text.sh: test for the text backend
+# Copyright 2009 by Vincent Fourmond 
+# This file is provided as an example of how to use ctioga2. As such,
+# you can do whatever you wish with this file.
+
+# Include the definition of ct
+. ./test-include.sh
+
+ruby ./generate-data.rb
+
+$ct -t "Parametric plot" --marker auto \
+    --text --margin 0.03 --xy-parametric --line-style no\
+    3d-data.dat@1:2:3
+
+$ct -t "Info through size" --marker auto \
+    --text --margin 0.03 --xy-parametric /z1=marker_scale \
+    --line-style no\
+    3d-data.dat@1:2:3
+
+$ct -t "Both size and colod" --marker auto \
+    --text --margin 0.03 --xy-parametric /z1=marker_scale /z2=marker_color \
+    --line-style no \
+    3d-data.dat@1:2:3:3
+
