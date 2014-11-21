582b1e101e697f039d97d5267eef5cf2665990ce
Vincent <vincent.fourmond@9online.fr>
2014-11-21 21:53:59 +0100

    [ctioga2] a bit more tests

--- tests/error-bars.sh
+++ tests/error-bars.sh
@@ -1,4 +1,4 @@
-# error-bars.sh: error bars
+# error-bars.sh: error bars and others
 # Copyright 2014 by Vincent Fourmond
 # 
 # You can do whatever you want with this file, including removing the
@@ -12,4 +12,6 @@
 $ct --math-samples 50 -L 't:t**2/10:sin(t**20)' -P /save=error-bars.dat
 
 $ct --text error-bars.dat@'$1:$2:yerr=$3' /legend 'Y error bars' \
-    error-bars.dat@'$1:$2+3:xerr=$3' /legend 'X error bars'
+    error-bars.dat@'$1:$2+3:xerr=$3' /legend 'X error bars' \
+    error-bars.dat@'$1:$2-3:$3:yerr=$3' /where 'z > 0.1' /legend 'With selection' \
+    error-bars.dat@'$1:$2-6:xerr=$3:$3**2:yerr=$3' /where 'z < 0.5' /legend 'Second With selection'
