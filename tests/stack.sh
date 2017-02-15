23da32b9aaf12aca2b296a3eed0956a4d3d813aa
Vincent <vincent.fourmond@9online.fr>
2017-02-16 00:50:19 +0100

    [ctioga2] ... and add a test so this will not happen again

--- tests/stack.sh
+++ tests/stack.sh
@@ -12,3 +12,14 @@ $ct --margin 0.03 \
     --join-datasets --plot-last \
     --load 'sin(x)+1' --append 'cos(x)+1' \
     --plot-last
+
+
+# Testing averages
+# All should be the same !
+$ct --math /samples=11 --margin 0.03 --marker auto \
+    --marker-set open --line-style no \
+    --plot 'sin(x)' \
+    --load 'sin(x)' \
+    --avg-dup-last --plot-last \
+    --load 'sin(x)' \
+    --join-datasets --sort-last --avg-dup-last --plot-last
